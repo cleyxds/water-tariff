03c718fbc1e4fbafd2ff29c6e8378253613361e2
Cleyson B <cleyxds@icloud.com>
2026-06-27 15:39:33 -0300

    init: initialize water-tariff project Spring Boot

---
+++ src/main/java/com/cleyxds/water_tariff/WaterTariffApplication.java
@@ -0,0 +1,13 @@
+package com.cleyxds.water_tariff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaterTariffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaterTariffApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cleyxds/water_tariff/WaterTariffApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cleyxds.water_tariff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaterTariffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
